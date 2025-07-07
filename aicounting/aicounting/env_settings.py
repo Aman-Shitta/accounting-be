51be92c614e8e36c5f3c16bd5e215ea0f448eec7
Aman-Shitta <amanshitta18@gmail.com>
2025-07-07 08:54:25 -0500

    fix: env settings varibale

--- aicounting/aicounting/env_settings.py
+++ aicounting/aicounting/env_settings.py
@@ -2,5 +2,5 @@ import os
 
 from dotenv import load_dotenv
 
-GEMINI_KEY = os.environ.get("GEMINI_API_KEY")
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
