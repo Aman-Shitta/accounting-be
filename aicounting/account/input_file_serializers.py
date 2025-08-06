a5bee2e139cf098d459a57ba4e0ada7a25233658
Aman-Shitta <amanshitta18@gmail.com>
2025-08-06 11:45:44 +0530

    fix: for input files type bank/cc make offsert account required.

--- aicounting/account/input_file_serializers.py
+++ aicounting/account/input_file_serializers.py
@@ -47,16 +47,35 @@ class InputFileListSerializer(serializers.ModelSerializer):
 class InputFileBasicCreateSerializer(serializers.ModelSerializer):
     """Serializer for creating input files without attributes"""
     
+    offset_gl_account = serializers.IntegerField(required=False, write_only=True, help_text="Offset GL Account ID for bank/credit card statements")
+    
     class Meta:
         model = DimAicInputFiles
         fields = [
-            'name', 'file_type', 'file', 'description'
+            'name', 'file_type', 'file', 'description', 'offset_gl_account'
         ]
+    
+    def validate(self, attrs):
+        """Validate offset_gl_account based on file_type"""
+        file_type = attrs.get('file_type')
+        offset_gl_account = attrs.get('offset_gl_account')
+        
+        # For bank statement and credit card, offset_gl_account is required
+        if file_type in ['bank_statement', 'credit_card']:
+            if not offset_gl_account:
+                raise serializers.ValidationError({
+                    'offset_gl_account': f'Offset GL Account is required for {file_type} files.'
+                })
+        
+        return attrs
     
     def create(self, validated_data):
         """Create input file without attributes"""
         request_user = self.context['request'].user
         client_id = self.context['client_id']
+        
+        # Extract offset_gl_account from validated_data
+        offset_gl_account_id = validated_data.pop('offset_gl_account', None)
         
         # Get the client instance
         try:
@@ -72,13 +91,22 @@ class InputFileBasicCreateSerializer(serializers.ModelSerializer):
         )
         
         # If file type is bank_statement or credit_card, create default attribute
-        if validated_data['file_type'] in ['bank_statement', 'credit_card']:
+        if input_file.file_type in ['bank_statement', 'credit_card']:
+            # Validate that the offset GL account exists
+            from account.models.dim_aic_gl_acct_model import DimAICGLAcct
+            try:
+                offset_gl_account = DimAICGLAcct.objects.get(id=offset_gl_account_id)
+            except DimAICGLAcct.DoesNotExist:
+                raise serializers.ValidationError({
+                    'offset_gl_account': 'Invalid offset GL account ID.'
+                })
+            
             DimAicInputFileAttributes.objects.create(
                 input_file=input_file,
                 name="*",  # Default name
                 gl_account=None,  # Null GL account for bank/credit card statements
                 type="",  # Empty type initially
-                offset_gl_account=None,  # Null offset GL account for bank/credit card statements
+                offset_gl_account=offset_gl_account,  # Required offset GL account
                 input_user=request_user,
                 comments="Auto-generated for bank statement/credit card processing"
             )
