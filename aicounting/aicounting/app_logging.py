72e2839ce2c4f6148e0138bedb651a1634430645
Aman-Shitta <amanshitta18@gmail.com>
2025-10-01 10:21:54 -0500

    fix: settings for proj

--- aicounting/aicounting/app_logging.py
+++ aicounting/aicounting/app_logging.py
@@ -1,112 +1,53 @@
-# import os
-# from django.conf import settings
-
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-
-#     'formatters': {
-#         'verbose': {
-#             'format': '[{asctime}] {levelname} {name} {message}',
-#             'style': '{',
-#         },
-#     },
-
-#     'handlers': {
-#         'info_file': {
-#             'level': 'INFO',
-#             'class': 'logging.FileHandler',
-#             'filename': os.path.join(settings.BASE_DIR, 'aicounting.log'),
-#             'formatter': 'verbose',
-#         },
-#         'error_file': {
-#             'level': 'ERROR',
-#             'class': 'logging.FileHandler',
-#             'filename': os.path.join(settings.BASE_DIR, 'error.log'),
-#             'formatter': 'verbose',
-#         },
-#     },
-
-#     'root': {
-#         'handlers': ['info_file', 'error_file'],
-#         'level': 'INFO',
-#     },
-
-#     'loggers': {
-#         'django': {
-#             'handlers': ['info_file', 'error_file'],
-#             'level': 'INFO',
-#             'propagate': True,
-#         },
-#         'django.request': {
-#             'handlers': ['error_file'],
-#             'level': 'ERROR',
-#             'propagate': False,
-#         },
-#         'django.server': {
-#             'handlers': ['info_file', 'error_file'],
-#             'level': 'INFO',
-#             'propagate': False,
-#         },
-#     },
-# }
+import os
+from django.conf import settings
 
 
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'console': {
-#             'class': 'logging.StreamHandler',
-#         },
-#     },
-#     'root': {
-#         'handlers': ['console'],
-#         'level': 'DEBUG',
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['console'],
-#             'level': 'INFO',  # You can set this to 'DEBUG' if you want Django internals too
-#             # 'propagate': False,
-#         },
-#         # Your app logger
-#         'aicounting': {
-#             'handlers': ['console'],
-#             'level': 'DEBUG',
-#             # 'propagate': False,
-#         },
-#     },
-# }
-
-
-import sys
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+
     'formatters': {
         'verbose': {
-            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+            'format': '[{asctime}] {levelname} {name} {message}',
+            'style': '{',
         },
     },
+
     'handlers': {
-        'console': {
-            'level': 'INFO',  # Or 'DEBUG' for more detailed logs
-            'class': 'logging.StreamHandler',
-            'stream': sys.stdout,
-            'formatter': 'verbose'
+        'info_file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(settings.BASE_DIR, 'aicounting.log'),
+            'formatter': 'verbose',
+        },
+        'error_file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(settings.BASE_DIR, 'error.log'),
+            'formatter': 'verbose',
         },
     },
+
+    'root': {
+        'handlers': ['info_file', 'error_file'],
+        'level': 'INFO',
+    },
+
     'loggers': {
-        '': {  # This is the root logger, catching all messages
-            'handlers': ['console'],
-            'level': 'INFO',  # Or 'DEBUG'
+        'django': {
+            'handlers': ['info_file', 'error_file'],
+            'level': 'INFO',
             'propagate': True,
         },
-        'django': { # Example for specific logger
-            'handlers': ['console'],
+        'django.request': {
+            'handlers': ['error_file'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.server': {
+            'handlers': ['info_file', 'error_file'],
             'level': 'INFO',
-            'propagate': True, # Prevent messages from being handled by the root logger again
+            'propagate': False,
         },
     },
 }
