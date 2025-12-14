9b13c4b201021939bfaf48789fd5a5fcfdab36c8
Aman-Shitta <amanshitta18@gmail.com>
2025-12-14 19:57:21 +0530

    feat: added API for refresh token

--- aicounting/authentication/urls.py
+++ aicounting/authentication/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import SSOLoginView, SSOGenerateTokenView
+from .views import (
+    SSOLoginView,
+    SSOGenerateTokenView,
+    SSORefreshTokenView
+)
 
 urlpatterns = [
     path('login/', SSOLoginView.as_view(), name='sso-login'),
     path('callback/', SSOGenerateTokenView.as_view(), name='sso-callback'),
+    path('refresh/', SSORefreshTokenView.as_view(), name='sso-refresh'),
+
 ]
