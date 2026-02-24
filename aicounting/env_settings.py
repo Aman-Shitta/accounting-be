1e5fc41da80872668b59ac9ef892aaaaa9dc26f8
Aman-Shitta <amanshitta18@gmail.com>
2026-02-24 19:06:33 +0530

    added sample env and env settings update

--- aicounting/env_settings.py
+++ aicounting/env_settings.py
@@ -26,12 +26,11 @@ OPENAI_PROJECT_ID = os.environ.get("OPENAI_PROJECT_ID")
 # Azure Storage Configuration
 AZURE_ACCOUNT_NAME = os.environ.get('AZURE_STORAGE_ACCOUNT_NAME')
 AZURE_ACCOUNT_KEY = os.environ.get('AZURE_STORAGE_ACCOUNT_KEY')
-AZURE_CONTAINER_NAME = os.environ.get(
-    'AZURE_CONTAINER_NAME', 'client-documents')
+AZURE_CONTAINER_NAME = os.environ.get('AZURE_CONTAINER_NAME')
 
 # Landing AI
 LANDING_AI_API_KEY = os.environ.get("LANDING_AI_API_KEY")
-LANDING_AI_ADE_MODEL = os.environ.get("LANDING_AI_ADE_MODEL", "dpt-2-latest")
+LANDING_AI_ADE_MODEL = os.environ.get("LANDING_AI_ADE_MODEL")
 
 # Document AI
 
